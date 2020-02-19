90d33189927af0245fb735e74238b1647a19ee08
Brett East <brett@playdraft.com>
2020-02-18 19:41:00 -0500

    add eslint basic config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    browser: true,
+  }
+}
